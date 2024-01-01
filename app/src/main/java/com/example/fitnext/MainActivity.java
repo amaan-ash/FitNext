b7a067346f5eaa36936bdf379f5bdec6398ad63d
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-01-02 00:03:02 +0530

    cm

--- app/src/main/java/com/example/fitnext/MainActivity.java
+++ app/src/main/java/com/example/fitnext/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.fitnext;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginBtnMain;
+    private Button registerBtnMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //rest of the code below
+        loginBtnMain=findViewById(R.id.loginBtnMain);
+        registerBtnMain=findViewById(R.id.registerBtnMain);
+
+        loginBtnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        registerBtnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
