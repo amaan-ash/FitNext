8e2e0d38248a9d3a0197cb2355aa190945635d26
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-01-02 22:01:01 +0530

    cm

--- app/src/main/java/com/example/fitnext/RegisterActivity.java
+++ app/src/main/java/com/example/fitnext/RegisterActivity.java
@@ -10,6 +10,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -24,6 +25,10 @@ public class RegisterActivity extends AppCompatActivity {
     private TextView goToSigninTextview;
     private Button registerBtn;
     private Button goToLoginBtn;
+
+    String email="";
+    String password="";
+    String confirmPassword="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +54,6 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
 
@@ -70,112 +74,130 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        //TextWatcher implementation for Email Input Field
+        textInputEmailRegister.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                validateEmail(charSequence.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        //TextWatcher implementation for Enter Password Field
+        textInputPasswordRegister.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                validateEnterPassword(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        //TextWatcher implementation for confirm password field
+        textConfirmPasswordRegister.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                validateConfirmPassword(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 
-    //this method is invoked when the user clicks on Register Here button
-    private void registerUser() {
-        String email=textInputEmailRegister.getText().toString().trim();
-        String password=textInputPasswordRegister.getText().toString().trim();
-        String confirmPassword=textConfirmPasswordRegister.getText().toString().trim();
-
-
-        if(email.isEmpty()){
-          textLayoutEmailRegister.setError("Email");
-           textLayoutEmailRegister.requestFocus();
-
-            textInputEmailRegister.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                    textLayoutEmailRegister.setError(null);
-                }
-
-                @Override
-                public void afterTextChanged(Editable editable) {
-
-                }
-            });
-            return;
-        }
-        else{
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                textLayoutEmailRegister.setError("Not a valid email Address");
-                textLayoutEmailRegister.requestFocus();
-                return;
-            }
-            else{
-                textLayoutEmailRegister.setError(null);
-            }
-        }
-        if(password.isEmpty()){
-            textLayoutPasswordRegister.setError("Choose Password");
-            textLayoutPasswordRegister.requestFocus();
-
-            textInputPasswordRegister.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                    textLayoutPasswordRegister.setError(null);
-                }
-
-                @Override
-                public void afterTextChanged(Editable editable) {
-
-                }
-            });
-            return;
-        }
-        if(confirmPassword.isEmpty()){
+    private void validateConfirmPassword(String confirmPassword) {
+        if (confirmPassword.isEmpty()) {
             textLayoutConfirmPasswordRegister.setError("Confirm Your Password");
             textLayoutConfirmPasswordRegister.requestFocus();
-
-            textConfirmPasswordRegister.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                     textLayoutConfirmPasswordRegister.setError(null);
-                }
-
-                @Override
-                public void afterTextChanged(Editable editable) {
-
-                }
-            });
             return;
         }
 
+        String password=textInputPasswordRegister.getText().toString().trim();
 
-        if (password.length() < 8) {
-            textLayoutPasswordRegister.setError("Minimum 8 characters");
-            textLayoutPasswordRegister.requestFocus();
-            return;
-        }
-        else {
-            textLayoutPasswordRegister.setError(null);
-        }
-
-        if(!password.equals(confirmPassword)){
+        if (!password.equals(confirmPassword)) {
             textLayoutConfirmPasswordRegister.setError("Passwords Do not Match");
             textLayoutConfirmPasswordRegister.requestFocus();
             return;
         }
-        else{
             textLayoutConfirmPasswordRegister.setError(null);
+
+    }
+
+    //this method is for validating the entered password
+    private void validateEnterPassword(String password) {
+        if (password.length() < 1) {
+            textLayoutPasswordRegister.setError("please enter password");
+            textLayoutPasswordRegister.requestFocus();
+            return;
+        } else if (password.length() < 8) {
+            textLayoutPasswordRegister.setError("minimum 8 characters required");
+            textLayoutPasswordRegister.requestFocus();
+            return;
+
         }
+        //clear the error if the password is valid
+        textLayoutPasswordRegister.setError(null);
+    }
+
+    //  this method for email validation
+    private void validateEmail(String email) {
+        if (email.isEmpty()) {
+            textLayoutEmailRegister.setError("email is required");
+            textLayoutEmailRegister.requestFocus();
+            return;
+        } else {
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                textLayoutEmailRegister.setError("not a valid email address");
+                textLayoutEmailRegister.requestFocus();
+                return;
+            }
+        }
+
+        // Clear the error if the email is valid
+        textLayoutEmailRegister.setError(null);
+    }
+
+    //this method is invoked when the user clicks on Register Here button
+    private void registerUser() {
+         email = textInputEmailRegister.getText().toString().trim();
+         password = textInputPasswordRegister.getText().toString().trim();
+         confirmPassword = textConfirmPasswordRegister.getText().toString().trim();
+
+
+
+
+
 
 
     }
+
 
 }
