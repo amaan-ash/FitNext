3bcce272492910cac0212e0fb85181275f0e6816
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-01-03 20:37:11 +0530

    cm

--- app/src/main/java/com/example/fitnext/DashBoard.java
+++ app/src/main/java/com/example/fitnext/DashBoard.java
@@ -1,10 +1,12 @@
 package com.example.fitnext;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -13,14 +15,40 @@ public class DashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
+        //the below is the replacement of the onBackPressed() method
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                showExitConfirmationDialog();
+            }
+        };
 
+        // Registering the callback
+        getOnBackPressedDispatcher().addCallback(this, callback);
 
+        //rest code of the onCreate() method
 
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        this.finishAffinity();
+
+    private void showExitConfirmationDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit")
+                .setMessage("Do you really want to exit the app?")
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // If the user clicks "Yes", exit the app
+                        finish();
+                    }
+                })
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // If the user clicks "No", do nothing and close the dialog
+                        dialog.dismiss();
+                    }
+                })
+                .setIcon(R.drawable.alert)
+                .show();
     }
 }
+
