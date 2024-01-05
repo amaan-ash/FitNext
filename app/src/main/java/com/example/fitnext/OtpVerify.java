fc0f8a490ae945caacbf8d3f66c53f95410f69df
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-01-05 10:48:58 +0530

    cm

--- app/src/main/java/com/example/fitnext/OtpVerify.java
+++ app/src/main/java/com/example/fitnext/OtpVerify.java
@@ -1,14 +1,120 @@
 package com.example.fitnext;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.chaos.view.PinView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class OtpVerify extends AppCompatActivity {
+    PinView pinView;
+    Button verifyCodeBtn;
+     FirebaseAuth auth;
+     String codeBySystem;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verify);
+
+        auth=FirebaseAuth.getInstance();
+
+        pinView=findViewById(R.id.pinView);
+        verifyCodeBtn=findViewById(R.id.verifyCodeBtn);
+
+        String phoneNumber= getIntent().getStringExtra("phoneNo");
+
+        verifyCodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendVerificationCode(phoneNumber);
+
+                String code=pinView.getText().toString();
+
+                if(!TextUtils.isEmpty(code)){
+                    verifyCode(code);
+                }
+            }
+        });
+
+        //implementing the callbacks
+        mCallbacks=new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                codeBySystem=s;
+            }
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                String code=phoneAuthCredential.getSmsCode();
+                if(code!=null){
+                    pinView.setText(code);
+                    verifyCode(code);
+                }
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(OtpVerify.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+    }
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential=PhoneAuthProvider.getCredential(codeBySystem,code);
+
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(OtpVerify.this, "Verification Successful", Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(OtpVerify.this, "Verification Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode(String phoneNumber) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(auth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // (optional) Activity for callback binding
+                        // If no activity is passed, reCAPTCHA verification can not be used.
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 }
--- app/src/main/java/com/example/fitnext/PhoneCallLogin.java
+++ app/src/main/java/com/example/fitnext/PhoneCallLogin.java
@@ -2,18 +2,26 @@ package com.example.fitnext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.hbb20.CountryCodePicker;
 
 public class PhoneCallLogin extends AppCompatActivity {
     TextInputEditText phoneNumberInput;
     TextInputLayout phoneNumberLayout;
+    FloatingActionButton nextBtn;
+    CountryCodePicker countryCodePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,9 @@ public class PhoneCallLogin extends AppCompatActivity {
         //getting views
         phoneNumberInput=findViewById(R.id.phoneNumberInput);
         phoneNumberLayout=findViewById(R.id.phoneNumberLayout);
+        nextBtn=findViewById(R.id.nextBtn);
+        countryCodePicker=findViewById(R.id.countryCodePicker);
+        countryCodePicker.registerCarrierNumberEditText(phoneNumberInput);
 
         // Set an InputFilter to allow only digits
         InputFilter inputFilter = new InputFilter() {
@@ -45,5 +56,37 @@ public class PhoneCallLogin extends AppCompatActivity {
 
         //rest of the code below
 
+        //setting the next Button
+                nextBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        callVerifyOtp();
+                    }
+                });
+
     }
+
+    private void callVerifyOtp() {
+        if (TextUtils.isEmpty(phoneNumberInput.getText())) {
+            phoneNumberLayout.setError("Enter Phone Number");
+            phoneNumberLayout.requestFocus();
+            return;
+        }
+        phoneNumberLayout.setError(null);
+        Toast.makeText(this, "first reach", Toast.LENGTH_SHORT).show();
+
+        String userEnteredNumber = phoneNumberLayout.getEditText().toString().trim();
+        Toast.makeText(this, "reached", Toast.LENGTH_SHORT).show();
+//        String phoneNumber="+"+countryCodePicker.getFullNumber()+userEnteredNumber;
+
+        Toast.makeText(this, " second reached", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "third reach", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(PhoneCallLogin.this, OtpVerify.class);
+        intent.putExtra("phoneNo",countryCodePicker.getFullNumberWithPlus().replace(" ",""));
+        Toast.makeText(this, "fourth reach", Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+    }
+
 }
