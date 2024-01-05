c4fd20695287fb7fa9d0db678d7c80b2bc81313a
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-01-05 21:27:56 +0530

    cm

--- app/src/main/java/com/example/fitnext/DashBoard.java
+++ app/src/main/java/com/example/fitnext/DashBoard.java
@@ -4,34 +4,31 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.firebase.Firebase;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class DashBoard extends AppCompatActivity {
-    private Button logoutBtn;
+public class DashBoard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     FirebaseAuth auth;
-
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
         auth= FirebaseAuth.getInstance();
-        logoutBtn=findViewById(R.id.logoutBtn);
 
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logout();
-            }
-        });
+
         //the below is the replacement of the onBackPressed() method
         OnBackPressedCallback callback = new OnBackPressedCallback(true) {
             @Override
@@ -45,8 +42,54 @@ public class DashBoard extends AppCompatActivity {
 
         //rest code of the onCreate() method
 
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId=item.getItemId();
+
+        if(itemId==R.id.nav_home){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        }
+        if(itemId==R.id.nav_about){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
+        }
+        if(itemId==R.id.nav_logout){
+           logout();
+        }
+        if(itemId==R.id.nav_settings){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+        }
+        if(itemId==R.id.nav_share){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ShareFragment()).commit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     private void logout() {
         new AlertDialog.Builder(this)
@@ -79,18 +122,19 @@ public class DashBoard extends AppCompatActivity {
                 .setMessage("Do you really want to exit the app?")
                 .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        // If the user clicks "Yes", exit the app
+                        // If the user clicks yes
                         finishAffinity();
                     }
                 })
                 .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        // If the user clicks "No", do nothing and close the dialog
+                        // If the user clicks no
                         dialog.dismiss();
                     }
                 })
                 .setIcon(R.drawable.alert)
                 .show();
     }
+
 }
 
