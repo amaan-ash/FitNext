2655c0f193f223ba7eb35bd0e3bcd60fd5caf50f
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-02-15 19:14:28 +0530

    commit

--- app/src/main/java/com/example/fitnext/LengthyExercisesFragment.java
+++ app/src/main/java/com/example/fitnext/LengthyExercisesFragment.java
@@ -3,16 +3,16 @@ package com.example.fitnext;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LengthyExercisesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LengthyExercisesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -34,7 +34,7 @@ public class LengthyExercisesFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LengthyExercisesFragment.
+     * @return A new instance of fragment have_time.
      */
     // TODO: Rename and change types and number of parameters
     public static LengthyExercisesFragment newInstance(String param1, String param2) {
@@ -55,10 +55,66 @@ public class LengthyExercisesFragment extends Fragment {
         }
     }
 
+    first f=new first();
+    second s=new second();
+    third t=new third();
+    Button b1,b2,b3;
+    ImageView ig1,ig2;
+    TextView tv1,tv2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lengthy_exercises, container, false);
+        View view=inflater.inflate(R.layout.fragment_lengthy_exercises, container, false);
+        ig1=view.findViewById(R.id.imageView13);
+        ig2=view.findViewById(R.id.imageView14);
+        tv1=view.findViewById(R.id.hvt1);
+        tv2=view.findViewById(R.id.hvt2);
+        ig1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,f);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,f);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        ig2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,s);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,f);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+
+        return view;
     }
 }
