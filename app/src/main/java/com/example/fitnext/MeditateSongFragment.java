4c6b684ec2504c39efe47138f60ac387dea11eaa
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-02-18 23:41:04 +0530

    commit

--- app/src/main/java/com/example/fitnext/MeditateSongFragment.java
+++ app/src/main/java/com/example/fitnext/MeditateSongFragment.java
@@ -59,6 +59,8 @@ public class MeditateSongFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meditate_song, container, false);
+        View view= inflater.inflate(R.layout.fragment_meditate_song, container, false);
+
+        return view;
     }
 }
