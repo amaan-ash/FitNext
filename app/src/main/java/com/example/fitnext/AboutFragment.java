8952f04070dca8d06276eeb7cb1ad1bfecd25cb3
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-02-21 12:16:54 +0530

    commit

--- app/src/main/java/com/example/fitnext/AboutFragment.java
+++ app/src/main/java/com/example/fitnext/AboutFragment.java
@@ -10,52 +10,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class AboutFragment extends Fragment {
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
-    private String mParam1;
-    private String mParam2;
 
     public AboutFragment() {
         // Required empty public constructor
     }
 
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_about, container, false);
-
-        TextView githublink;
-
-        githublink=view.findViewById(R.id.githublink);
-        githublink.setPaintFlags(githublink.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        githublink.setOnClickListener(new View.OnClickListener() {
+        TextView gitHubLink =view.findViewById(R.id.githublink);
+        gitHubLink.setPaintFlags(gitHubLink.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        gitHubLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String githubUrl = "https://github.com/amaan0707";
--- app/src/main/java/com/example/fitnext/amazonFragment.java
+++ app/src/main/java/com/example/fitnext/amazonFragment.java
@@ -14,13 +14,6 @@ import android.widget.Button;
 
 public class amazonFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public amazonFragment() {
         // Required empty public constructor
     }
@@ -28,22 +21,9 @@ public class amazonFragment extends Fragment {
 
 
     public static amazonFragment newInstance(String param1, String param2) {
-        amazonFragment fragment = new amazonFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+           return new amazonFragment();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
