eac3dd45e9b7455ce77a62f5fb848cb96d41d0d4
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2024-03-13 20:34:52 +0530

    commit

--- app/src/main/java/com/example/chatgpt/API.java
+++ app/src/main/java/com/example/chatgpt/API.java
@@ -2,5 +2,5 @@ package com.example.chatgpt;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "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";
 }
